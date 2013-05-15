3f3ab16e3faa886a4a2fb4edfcc57f6613121519
Spuds <spuds@spudsdesign.com>
2013-05-15 10:38:07 -0500

    ! Move to MPL v2
    Signed-off-by: Spuds <spuds@spudsdesign.com>

--- sources/GoogleMapIntegration.php
+++ sources/GoogleMapIntegration.php
@@ -4,8 +4,10 @@
  *
  * @package "Google Member Map" Mod for Simple Machines Forum (SMF) V2.0
  * @author Spuds
- * @copyright (c) 2011 Spuds
- * @license license.txt (included with package) BSD
+ * @copyright (c) 2011-2013 Spuds
+ * @license This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
  *
  * @version 2.6
  *
